32e7cd8d8aa47ac6ce689e605d59cf77a727ff85
hans <Hans Brenna>
2018-04-06 09:13:23 +0200

    Worked on parallelization of precision measurement

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import gzip
+import threading
 import argparse
 from operator import itemgetter
 from tools.rater import rate_article
@@ -85,6 +86,7 @@ for title in keys:
         del user_articles[title]
 print(len(article_keywords.keys()))
 print(len(user_articles.keys()))
+print("Finished disk reading, time for them numbers")
 article_corpus = []
 article_tags = []
 for title in article_keywords:
@@ -105,39 +107,82 @@ for user in user_keywords:
     user_corpus.append(user_keywords[user])
     user_tags.append(user)
 tf_idf_users = vectorizer.fit_transform(user_corpus)
-similarity_matrix = linear_kernel(tf_idf_articles,tf_idf_users[2])
-
-user_tag = user_tags[2]
-ratings = []
-"""
-for counter in range(0,similarity_matrix.shape[0]):
-    if similarity_matrix[0][counter]!=0:
-        ratings.append((article_tags[counter],similarity_matrix[0][counter]))
-print(ratings)
-"""
-
-for entry in range(0,similarity_matrix.shape[0]):
-    if similarity_matrix[entry][0]!=0:
-        ratings.append((article_tags[entry],similarity_matrix[entry][0]))
-print(user_tag)
-print(ratings)
-ratings = sorted(ratings,key=itemgetter(1),reverse=True)
-print(ratings)
-global_relevant = 0
-for title,score in ratings:
-    if user_tag in user_articles[title]:
-        global_relevant+=1
-global_relevant = float(global_relevant)
-n_retrieved = []
-max_rating = repr(args.level)
-for counter in range(0,max_rating):
-    n_retrieved.append(ratings[counter])
-
-
-local_relevant = 0
-for title,score in n_retrieved:
-    if user_tag in user_articles[title]:
-        local_relevant+=1
+similarity_matrix = linear_kernel(tf_idf_articles,tf_idf_users)
+def calculate_cosine_sim(uid,k_value):
+        
     
-presicion = local_relevant/max_rating
-print(presicion)
+
+    user_tag = user_tags[uid]
+    ratings = []
+
+
+    for entry in range(0,similarity_matrix.shape[0]):
+        if similarity_matrix[entry][uid]!=0:
+            ratings.append((article_tags[entry],similarity_matrix[entry][uid]))
+    ratings = sorted(ratings,key=itemgetter(1),reverse=True)
+    global_relevant = 0
+    for title,score in ratings:
+        if user_tag in user_articles[title]:
+            global_relevant+=1
+    global_relevant = float(global_relevant)
+    n_retrieved = []
+    max_rating = k_value
+    for counter in range(0,max_rating):
+        if not len(ratings)<=counter:
+            n_retrieved.append(ratings[counter])
+
+
+    local_relevant = 0
+    for title,score in n_retrieved:
+        if user_tag in user_articles[title]:
+            local_relevant+=1
+        
+    presicion = local_relevant/max_rating
+    return presicion
+calculate_cosine_sim(1,10)
+total_precision = 0
+add_lock = threading.Lock()
+tracer = 0
+for counter in range(0,similarity_matrix.shape[1]):
+    total_precision+=calculate_cosine_sim(counter,5)
+    if counter>tracer:
+        print("Finished "+repr(counter)+" vectors , "+repr(similarity_matrix.shape[1]-counter)+" remaining")
+        tracer+=10000
+
+
+
+class myThread (threading.Thread):
+    def __init__(self, threadID,chunk_size):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.chunk_size = chunk_size
+      self.offset = self.threadID*self.chunk_size
+      self.range = self.offset+self.chunk_size
+      self.local_precision = 0
+    def run(self):
+        tracer = 100
+        for counter in range(self.offset,self.range):
+            self.local_precision+=calculate_cosine_sim(counter,10)
+            if((counter-self.offset)>tracer):
+                print("Thread "+repr(self.threadID)+"finished "+repr(counter-self.offset)+" vectors "+repr(chunk_size-(counter-self.offset))+" to go")
+                tracer+=100
+        add_lock.acquire()
+        global total_precision
+        total_precision+=self.local_precision
+        add_lock.release()
+
+
+num_threads = 16
+chunk_size = int(similarity_matrix.shape[1]/num_threads)
+threads = []
+"""
+for counter in range(0,num_threads):
+    thread = myThread(counter,chunk_size)
+    threads.append(thread)
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+"""
+print(total_precision)
+print(total_precision/similarity_matrix.shape[1])
