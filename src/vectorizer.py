5c8f56f2274a0d7412f429028c8bc38e5c6cd89c
hans <Hans Brenna>
2018-04-11 21:01:39 +0200

    Doing some cleaning in vectorizer

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -116,6 +116,30 @@ tf_idf_users = vectorizer.fit_transform(user_corpus)
 similarity_matrix = linear_kernel(tf_idf_articles,tf_idf_users)
 
 recommendation_count = dict()
+
+def calculate_roc(intervals,ratings,global_relevant_titles):
+    roc = []
+    for i in range(0,intervals):
+        score_tres = i*0.1
+        positives = len(global_relevant_titles)
+        right_positives = 0
+        right_negatives = 0
+        negatives = len(ratings) - len(global_relevant_titles)
+        for title,score in ratings:
+            
+            
+            if title in global_relevant_titles:
+                if score_tres<score:
+                    right_positives+=1
+            else:
+                
+                if score_tres<score:
+                    right_negatives+=1
+        res_true = float(right_positives)/float(positives) if positives!=0 else 0
+        res_false = float(right_negatives)/float(negatives) if negatives!=0 else 0
+        roc.append((res_true,res_false))
+    return roc
+
 def calculate_cosine_sim(uid,k_value,r_count):
     
     user_tag = user_tags[uid]
@@ -134,6 +158,11 @@ def calculate_cosine_sim(uid,k_value,r_count):
     n_retrieved = []
     max_rating = k_value
 
+    
+        
+
+            
+
     for counter in range(0,max_rating):
         if not len(ratings)<=counter:
             n_retrieved.append(ratings[counter])
@@ -141,104 +170,99 @@ def calculate_cosine_sim(uid,k_value,r_count):
 
     local_relevant = 0
     local_irrelevant = 0
-    true_p = dict()
-    false_p = dict()
     local_relevant_titles = []
+    mrhr = 0
+    rank = 1
     for title,score in n_retrieved:
         if title in global_relevant_titles:
             local_relevant+=1
             local_relevant_titles.append(title)
+            mrhr = float(1/rank) if mrhr == 0 else mrhr
         else:
             local_irrelevant+=1
-        true_p[score] = float(local_relevant)/len(n_retrieved)
-        false_p[score] = float(local_irrelevant)/len(n_retrieved)
-        
         if title not in r_count:
             r_count[title] = 0
         r_count[title]+=1
-    presicion = local_relevant/max_rating
+        rank+=1
 
-total_precision = 0
+    presicion = local_relevant/max_rating
+    recall = local_relevant/global_relevant
+    roc = calculate_roc(10,ratings,global_relevant_titles)
+    return (recall,presicion,mrhr,roc)
+
+
 
 tracer = 10000
 total_ctr = 0
 
-def calculate_total_roc(true_pr,false_pr,tot_roc,step_size):
-    steps = len(tot_roc)
-    rates = []
-    for counter in range(0,steps):
-        limit = counter*step_size
-        local_true = 0
-        local_false = 0
-        false_rate = 0
-        true_rate = 0
-        for score in true_pr:
-            if  limit<score:
-                true_rate+=true_pr[score]
-                local_true+=1
-        for score in false_pr:
-            if limit < score:
-                false_rate+=false_pr[score]
-                local_false+=1
-        res_true = float(true_rate/local_true) if local_true!=0 else 0
-        res_false = float(false_rate/local_false) if local_false!=0 else 0
-        
-        rates.append((res_true,res_false))
-    if not len(tot_roc)==0:
-        for i in range(0,len(tot_roc)):
-            tot_roc[i] = (tot_roc[i][0]+rates[i][0],tot_roc[i][1]+rates[i][1])
-        return tot_roc
-    else:
-        return rates
+def sum_roc(new_roc,old_roc):
+    if len(old_roc)!=0:
+        for i in range(0,len(new_roc)):
+            new_roc[i] = ((new_roc[i][0]+old_roc[i][0]),new_roc[i][1]+old_roc[i][1])
+    return new_roc
+
+def avg_roc(roc,n):
+    avg_roc = []
+    for entry in roc:
+        avg_roc.append((entry[0]/n,entry[1]/n))
+    return avg_roc
+
 def calculate_aoc(roc):
     area_under = 0
     for i in range(0,len(roc)-1):
-        (x0,y0) = roc[i]
-        (x1,y1) = roc[i+1]
+        (y0,x0) = roc[i]
+        (y1,x1) = roc[i+1]
         dx = x0-x1 
         dy = y0-y1
-        area_under+=(dx*dy)
-        print('dx',dx,'dy',dy,'area under',area_under)
+        area_under+= ((dx*dy)/2)+dx*y0
         x0 = x1
         y0 = y1
-    area = roc[0][0]*roc[0][1]
-    return area_under/area
+    return area_under/1.0
 
-
-
-
-
-        
-
-                
-
-    
-    
-
-if not args.parallel:
-    total_roc = []
-    for counter in range(0,10):
-        total_roc.append((0,0))
-    for counter in range(0,similarity_matrix.shape[1]):
-        (local_precision,true_p_r,false_p_r) = calculate_cosine_sim(counter,5,recommendation_count)
-        total_roc = calculate_total_roc(true_p_r,false_p_r,total_roc,0.1)
-        total_precision+=local_precision
-
-        if counter>tracer:
-            print("Finished "+repr(counter)+" vectors , "+repr(similarity_matrix.shape[1]-counter)+" remaining")
-            
-            #tracer+=10000
+def calculate_total_ctr(user_articles,recommendation_count):
+    total_ctr = 0
     for title in recommendation_count:
         total_ctr+= float(len(user_articles[title]))/float(recommendation_count[title])
-    average_ctr = total_ctr/similarity_matrix.shape[0]
-    average_roc = []
-   
-    for (true_r,false_r) in total_roc:
-        average_roc.append((float(true_r/similarity_matrix.shape[1]),float(false_r/similarity_matrix.shape[1])))
-    print("ROC")
-    for (true_r,false_r) in average_roc:
-        print("True",true_r,"False",false_r)
-    print("ROC",calculate_aoc(average_roc))
-    
-    print("Avg. Precision",total_precision/similarity_matrix.shape[1])
-    print("CTR",average_ctr)
+    return total_ctr
+
+
+
+total_roc = []
+for counter in range(0,10):
+    total_roc.append((0,0))
+total_precision = 0
+total_recall = 0
+total_mrhr = 0
+
+for counter in range(0,similarity_matrix.shape[1]):
+    (local_recall,local_precision,local_mrhr,roc) = calculate_cosine_sim(counter,5,recommendation_count)
+    total_precision+=local_precision
+    total_roc = sum_roc(roc,total_roc)
+    total_recall+=local_recall
+    total_mrhr+=local_mrhr
+
+    if counter>tracer:
+        print("Finished "+repr(counter)+" vectors , "+repr(similarity_matrix.shape[1]-counter)+" remaining")
+        
+        #tracer+=10000
+
+
+
+
+total_ctr = calculate_total_ctr(user_articles,recommendation_count)
+average_ctr = total_ctr/similarity_matrix.shape[0]
+average_roc = avg_roc(total_roc,similarity_matrix.shape[1])
+average_precision = total_precision/similarity_matrix.shape[1]
+average_recall = total_recall/similarity_matrix.shape[1]
+average_mrhr = total_mrhr/similarity_matrix.shape[1]
+averarage_f1 = (2*average_precision*average_recall)/(average_precision+average_recall)
+print("ROC")
+for (true_r,false_r) in average_roc:
+    print("True",true_r,"False",false_r)
+print("AOC",calculate_aoc(average_roc))
+print('Avg. Recall',average_recall)
+print('Avg. MRHR', average_mrhr)
+print("Avg. Precision",average_precision)
+print('Avg. f1', averarage_f1)
+print('Avg. CTR', average_ctr)
+
